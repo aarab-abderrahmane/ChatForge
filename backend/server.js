e93bfb0bf6552d61dd84458d33cea86e3fc980c1
aarab-abderrahmane <abdulrahman.tie@gmail.com>
2025-11-21 00:29:02 +0100

    changes

--- backend/server.js
+++ backend/server.js
@@ -186,4 +186,5 @@ app.get("/api/key-check",(_,res)=>{
 
 })
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+// app.listen(5000, () => console.log("Server running on port 5000"));
+export default app;
