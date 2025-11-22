75a72237261ac49dfb22cc4a0b31b14322879b56
aarab-abderrahmane <abdulrahman.tie@gmail.com>
2025-11-22 01:50:34 +0100

    understand error 3

--- backend/api/test.js
+++ backend/api/test.js
@@ -1,7 +1,8 @@
 import { askAI, encrypt, saveUserKey } from './functions.js';
 
 export default async function handler (req,res){
-
+  console.log("Test endpoint hit");
+  res.status(200).json({ message: "Server is running!" });
   if (req.method !== 'POST') return res.status(405).end();
 
 
